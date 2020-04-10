d77142cb9b54ea9321c0f8b27597c451aa188d19
futorimoyashi <jamekin196@gmail.com>
2020-04-10 23:05:23 +0900

    use Pug template engine

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'pug');
+app.set('views', './views')
+
 app.get('/', function(req, res) {
-	res.send('Hello World');
+	res.render('index', {
+		name: 'Kin nek'
+	});
 });
+
+app.get('/users', function(req, res) {
+	res.render('user', {
+		users: [
+			{id: 1, name: 'Kin nek'},
+			{id: 2, name: 'Nhi nek'},
+			{id: 3, name: 'Na nek'}
+		]
+	})
+})
 
 app.listen(port, function() {
 	console.log('Application is running on 8080 port')
