ca8e110c74c03278aa7f5ba5c71f35b9f6c2963f
futorimoyashi <jamekin196@gmail.com>
2020-04-11 23:02:36 +0900

    Demo Req.body and Req.query

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug');
 app.set('views', './views')
+
+var users = [
+	{id: 1, name: 'tung'},
+	{id: 2, name: 'nhi'},
+	{id: 3, name: 'benadangyeu'}
+];
 
 app.get('/', function(req, res) {
 	res.render('index', {
@@ -13,12 +23,29 @@ app.get('/', function(req, res) {
 
 app.get('/users', function(req, res) {
 	res.render('user', {
-		users: [
-			{id: 1, name: 'Kin nek'},
-			{id: 2, name: 'Nhi nek'},
-			{id: 3, name: 'Na nek'}
-		]
+		users: users
 	})
+});
+
+app.get('/users/search', function(req, res) {
+	var q = req.query.q;
+
+	var matchedUsers = users.filter(function(user) {
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+
+	res.render('user', {
+		users: matchedUsers
+	})
+});
+
+app.get('/users/create', function(req, res) {
+	res.render('create');
+});
+
+app.post('/users/create', function(req, res) {
+	users.push(req.body);
+	res.redirect('/users');
 })
 
 app.listen(port, function() {
