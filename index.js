73ed9d00bb3834b6bc7c399891489f128e7843d1
futorimoyashi <jamekin196@gmail.com>
2020-04-13 23:13:53 +0900

    route

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const low = require('lowdb');
-const FileSync = require('lowdb/adapters/FileSync');
-const shortid = require('shortid');
-const adapter = new FileSync('db.json');
-const db = low(adapter);
 
-db.defaults({ users: [] })
-  .write()
+const userRoute = require('./routes/user.route');
 
 const port = 8080;
 
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.set('view engine', 'pug');
-app.set('views', './views')
 
 app.get('/', function(req, res) {
 	res.render('index', {
@@ -24,41 +18,7 @@ app.get('/', function(req, res) {
 	});
 });
 
-app.get('/users', function(req, res) {
-	res.render('user', {
-		users: db.get('users').value()
-	})
-});
-
-app.get('/users/search', function(req, res) {
-	var q = req.query.q;
-
-	var matchedUsers = db.get('users').value().filter(function(user) {
-		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-	});
-
-	res.render('user', {
-		users: matchedUsers
-	})
-});
-
-app.get('/users/create', function(req, res) {
-	res.render('create');
-});
-
-app.get('/users/:id', function(req, res) {
-	var id = req.params.id;
-	var user = db.get('users').find({ id: id}).value();
-	res.render('view', {
-		user: user
-	});
-});
-
-app.post('/users/create', function(req, res) {
-	req.body.id = shortid.generate();
-	db.get('users').push(req.body).write();
-	res.redirect('/users');
-})
+app.use('/users', userRoute);
 
 app.listen(port, function() {
 	console.log('Application is running on 8080 port')
